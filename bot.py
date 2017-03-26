5470dd8589e05b18d31b0dc0267f3dda1925e9f2
Takesha <graukin@gmail.com>
2017-03-26 17:21:34 +0300

    m

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@
 import logging
 import os
 import sys
+import random
+import re
 import telebot
 
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
@@ -10,13 +12,22 @@ from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARN)
 logger = logging.getLogger(__name__)
 
+def dice_roll(matchobj):
+    x,y=map(int,matchobj.groups())
+    s = 0
+    for each in range(x):
+        s += random.randint(1, y)
+    return '(' + str(s) + ')'
+
 def talk(bot, update):
     logger.info('Update [%s]' % (update))
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm crazy purple unicorn!!!!!")
 
 def roll(bot, update):
     logger.info('Update [%s]' % (update))
-    update.message.reply_text(update.message)
+    msg = update.message.text
+    res=re.sub('(\d+)d(\d+)', dice_roll, msg)
+    update.message.reply_text('%s = %s', % (res, eval(res)))
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
