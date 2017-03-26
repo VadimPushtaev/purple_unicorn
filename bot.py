bbd8aaa74e16367d7122a2583df7298e1ed966f1
Takesha <graukin@gmail.com>
2017-03-26 16:11:17 +0300

    m

--- bot.py
+++ bot.py
@@ -9,28 +9,34 @@ import telebot
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARN)
 logger = logging.getLogger(__name__)
 
-config_name=sys.argv[1];
-config = ConfigParser.ConfigParser()
-config.read(config_name)
-
-TOKEN = config.get('main', 'token')
-PORT = int(os.environ.get('PORT', '5000'))
-
-UPDATER = Updater(token=TOKEN)
-DISPATCHER = UPDATER.dispatcher
-
-def start(bot, update):
-    logger.info("Update [" + update + "]")
+def talk(bot, update):
+    logger.info('Update [%s]' % (update))
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm crazy purple unicorn!!!!!")
 
-if __name__ == '__main__':
-    start_handler = CommandHandler('start', start)
-    DISPATCHER.add_handler(start_handler)
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
 
-    UPDATER.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    UPDATER.bot.setWebhook("https://punic.herokuapp.com/" + TOKEN)
-    UPDATER.idle()
+if __name__ == '__main__':
+    # get config info
+    config_name=sys.argv[1];
+    config = ConfigParser.ConfigParser()
+    config.read(config_name)
+
+    TOKEN = config.get('main', 'token')
+    updater = Updater(token=TOKEN)
+
+    PORT = int(os.environ.get('PORT', '5000'))
+
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    updater.bot.setWebhook("https://punic.herokuapp.com/" + TOKEN)
+    
+    # get dispatcher
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('talk', talk))
+    dispatcher.add_error_handler(error)
+
+    updater.idle()
 
