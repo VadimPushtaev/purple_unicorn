519cc1e736022336d2c7519a01eabaf7c61aed6c
graukin <graukin@gmail.com>
2018-03-02 18:47:50 +0300

    test for init command

--- test_bot.py
+++ test_bot.py
@@ -20,17 +20,17 @@ class BotTestCase(TestCase):
 
     def test_get_random_message(self):
         PurpleBot._get_rand = lambda self, max: 0
-        self.assertEquals("I'm crazy purple unicorn!!!!!", self.bot.get_random_greetings("ABC"))
+        self.assertEqual("I'm crazy purple unicorn!!!!!", self.bot.get_random_greetings("ABC"))
         PurpleBot._get_rand = lambda self, max: 4
-        self.assertEquals('ABC, why are you talking to me?!', self.bot.get_random_greetings("ABC"))
+        self.assertEqual('ABC, why are you talking to me?!', self.bot.get_random_greetings("ABC"))
 
     def test_init(self):
         PurpleBot._get_rand = lambda self, max: ((yield  0), (yield  3), (yield 5), (yield 8))
 
-        self.assertEquals('Results:\n' + \
+        self.assertEqual('Results:\n' + \
                           '<code>abcde</code> : <b>8</b> (5 3 [8])\n' + \
                           '<code>cd   </code> : <b>8</b> (0 8 [3])\n' + \
-                          '<code>a    </code> : <b>-5</b> (0 -5 [3])\n', self.bot.generate_init([("a", -5), ("abcde", 3), ("cd", 8)]))
+                          '<code>a    </code> : <b>-5</b> (0 -5 [3])\n', self.bot.generate_init([("a", "-5"), ("abcde", "3"), ("cd", "8")]))
 
         # a 0 (-5) 3 -> -5 #3
         # abcde 5 (+3) 8 -> 8 #1
