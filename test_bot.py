6385ae83487773a4eb1e94315cfa0e975ec32532
graukin <graukin@gmail.com>
2018-03-02 18:30:07 +0300

    add tests

--- test_bot.py
+++ test_bot.py
@@ -12,10 +12,20 @@ class BotTestCase(TestCase):
     bot = PurpleBot()
 
     def setUp(self):
-        PurpleBot._get_rand = lambda self, max: int(max/2)
+        PurpleBot._get_rand = lambda self, max: int(max)
 
     def test_roll(self):
+        PurpleBot._get_rand = lambda self, max: 9
         self.assertEqual(10, self.bot._get_roll(20))
+
+    def test_get_random_message(self):
+        PurpleBot._get_rand = lambda self, max: 0
+        self.assertEquals("I'm crazy purple unicorn!!!!!", self.bot.get_random_greetings("ABC"))
+        PurpleBot._get_rand = lambda self, max: 4
+        self.assertEquals('ABC, why are you talking to me?!', self.bot.get_random_greetings("ABC"))
+
+    def test_init(self):
+        PurpleBot._get_rand = lambda self, max: ((yield  0), (yield  3), (yield 5), (yield 8))
 
 
 if __name__ == '__main__':
